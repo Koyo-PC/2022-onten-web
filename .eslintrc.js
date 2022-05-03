b1c4b0d2e40b2aa803592000a66a6aa363c9dbc2
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-05-03 17:58:41 +0900

    Prepare for dev + Add JetBrains Settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
@@ -20,5 +21,7 @@ module.exports = {
         "vue",
         "@typescript-eslint"
     ],
-    "rules": {}
+    "rules": {
+        'vue/multi-word-component-names': 'off'
+    }
 };
