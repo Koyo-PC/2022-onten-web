bca1040f140c5cfaf39fe53aebb3c8c1aa4c0297
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-05-09 22:49:30 +0900

    Add map images & builder

---
+++ public_asset/img/map/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+public_path="../../../public"
+cd `dirname $0`
+for i in *.png; do ffmpeg -y -i "$i" -q 80 -vf "format=gray,curves=master='0|0 0.7|0 0.825|1 1|1',smartblur=lr=1:ls=0.5" "$public_path/img/map/${i%.*}.webp"; done
