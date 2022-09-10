5fddf8d57f197d3176b98f5bf9975687d062a089
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-09-10 15:08:22 +0900

    make band page

--- pages/circle/band.vue
+++ pages/circle/band.vue
@@ -2,47 +2,48 @@
   <div>
     <CircleHead name="band" />
     <p>
-      今年は講堂でライブ映像の放映、そして3年ぶりの生演奏を行います！熱い音楽を見逃すな！ぜひ講堂へお越しください！
+      今年は講堂でライブ映像の放映、そして3年ぶりの生演奏を行います！熱い音楽を見逃すな！ぜひ講堂へお越しください！<br />
     </p>
+    <p><a href="https://www.instagram.com/koyo.keion">公式Instagram</a></p>
     <h2>【バンド紹介】</h2>
-    <ul>
+    <ul id="band-list">
       <li>
-        セントラルドグマ<br />
-        We are the rock．<br />
-        (Vo．ユータン　Gt．宮田　Ba.もやし　Dr．ダンボ　key.Catherine)
+        <span class="band-name">セントラルドグマ</span>
+        We are the rock.<br />
+        (Vo. ユータン Gt. 宮田 Ba. もやし Dr. ダンボ key. Catherine)
       </li>
       <li>
-        The philosopher`s stones<br />
-        魂のrock ‘n’ rollをあなたに…<br />
-        (Vo＆Gt．佐田一真　Gt．竹澤友真　Ba.田中康太郎　Dr．木本裕也　Ch．原英貴)
+        <span class="band-name">The philosopher`s stones</span>
+        魂のrock 'n' rollをあなたに...<br />
+        (Vo&Gt. 佐田一真 Gt. 竹澤友真 Ba. 田中康太郎 Dr. 木本裕也 Ch. 原英貴)
       </li>
       <li>
-        The silent picus<br />
+        <span class="band-name">The silent picus</span>
         Good music must be an innovation.<br />
-        (Vo＆Gt．原英貴　Gt．玉川稜　Gt．佐田一真　Ba＆Vo.　新堂源太　Dr．大塚楓月　Key.岡本健伸)
+        (Vo&Gt. 原英貴 Gt. 玉川稜 Gt. 佐田一真 Ba&Vo. 新堂源太 Dr. 大塚楓月 Key.
+        岡本健伸)
       </li>
       <li>
-        Sifica<br />
-        106回生(１年生)の6人編成のバンド！<br />
-        あの“夏の定番曲”をカバーします！<br />
+        <span class="band-name">Sifica</span>
+        106回生(1年生)の6人編成のバンド！<br />
+        あの"夏の定番曲"をカバーします！<br />
         レコーディング、撮影、編集は目も耳も楽しめるように凝ったものにしました！ぜひお越しください！！<br />
-        (Vo．田畑聡大、木戸裕太　Gt．信田大雅　Ba．西本匠　Dr．福島令旺　Key．林優寿)
+        (Vo. 田畑聡大,木戸裕太 Gt. 信田大雅 Ba. 西本匠 Dr. 福島令旺 Key. 林優寿)
       </li>
       <li>
-        チトセ兄さん<br />
+        <span class="band-name">チトセ兄さん</span>
         技術はまだまだの1年生バンドですが、たくさん練習して頑張りました！<br />
-        どうか温かい目でお願いします…<br />
-        (Vo＆Gt．大槻明澄　Gt．糸瀬智基　Ba．大原尚士　Dr．阿比留将)
+        どうか温かい目でお願いします...<br />
+        (Vo&Gt. 大槻明澄 Gt. 糸瀬智基 Ba. 大原尚士 Dr. 阿比留将)
       </li>
       <li>
-        福山和之<br />
+        <span class="band-name">福山和之</span>
         福山雅治さんのものまねをしています。<br />
         皆さんにとっても僕らにとっても明日を照らす「光」となるライブをお届けしたいと思います。<br />
         ライブ会場では皆でひとつになりましょうね！！<br />
         (Vo＆Gt．福山和之)
       </li>
     </ul>
-    <a href="https://www.instagram.com/koyo.keion">公式Instagram</a>
   </div>
 </template>
 <script setup lang="ts">
@@ -58,4 +59,18 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#band-list {
+  margin-top: 20px;
+  padding-left: 20px;
+  li {
+    margin: 20px 0;
+    .band-name {
+      display: block;
+      margin-bottom: 10px;
+      font-size: 1.2rem;
+      font-weight: 700;
+    }
+  }
+}
+</style>
