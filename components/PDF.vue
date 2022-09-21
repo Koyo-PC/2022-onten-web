e4b5c0751b3c73dbeaa72e883707a275c9c30ae8
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-09-21 23:37:08 +0900

    add pdf download button

--- components/PDF.vue
+++ components/PDF.vue
@@ -4,6 +4,7 @@
       id="pdf-content"
       :src="`/lib/pdfjs-2.16.105-dist/web/viewer.html?file=${src}&zoom=page-fit`"
     />
+    <a class="pdf-download" :href="src" download>ダウンロード</a>
   </div>
 </template>
 <script setup lang="ts"></script>
@@ -22,13 +23,23 @@ export default {
 <style lang="scss" scoped>
 .pdf {
   position: relative;
+  width: 100%;
   height: 60vh;
-  aspect-ratio: calc(1 / math.sqrt(2));
-  margin: 0 auto;
   #pdf-content {
     width: 100%;
-    height: 100%;
+    height: calc(100% - 40px);
     object-fit: contain;
+  }
+  .pdf-download {
+    display: block;
+    width: 10em;
+    height: 40px;
+    color: black;
+    text-align: center;
+    background-color: #e5e5e5;
+    padding: 10px;
+    margin: 10px auto;
+    text-decoration: none;
   }
 }
 </style>
